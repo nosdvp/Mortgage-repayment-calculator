ad3f4c4ef2e57b4d5a900b872d0c47fe72ee1044
Andrew Mischenko <mishchenkoa.97@gmail.com>
2026-06-30 09:23:55 +0300

    Поработал над версткой блока введения информации, создал поля ввода суммы кредита + срока действия кредита + процентной ставки, добавил анимирования

--- src/screens/InputData/InputData.js
+++ src/screens/InputData/InputData.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import './InputData.css'
+import calcImg from '../../img/icon-calculator.svg'
 
 const InputData = ({change}) => {
 
@@ -11,9 +13,51 @@ const InputData = ({change}) => {
   }
 
   return (
-    <div>
-      <button onClick={changeFormShowData}>Calculate Repaymants</button>
-      <button onClick={clearAll}>ClearAll</button>
+    <div className='InputData-wrapper'>
+      <div className='InputData-header'>
+        <div className='InputData-header-title'>Mortgage Calculator</div>
+        <button className='InputData-header-clearAll' onClick={clearAll}>Clear All</button>
+      </div>
+
+      <div className='InputData-enterDataBlock'>
+        <div className='InputData-enterDataBlock-title'>Mortgage Amount</div>
+        <div className='InputData-enterDataBlock-inputBlock'>
+          <div className='InputData-enterDataBlock-inputBlock-iconAmount'>
+            <div className='InputData-enterDataBlock-inputBlock-iconAmount-icon'>£</div>
+          </div>
+          <input className='InputData-enterDataBlock-inputBlock-input' placeholder='0.00'></input>
+        </div>
+      </div>
+
+      <div className='InputData-addDataBlock'>
+        <div className='InputData-addDataBlock-term'>
+          <div className='InputData-addDataBlock-term-title'>Mortgage Term</div>
+          <div className='InputData-addDataBlock-term-inputBlock'>
+            <input className='InputData-addDataBlock-term-inputBlock-input'></input>
+            <div className='InputData-addDataBlock-term-inputBlock-years'>
+              <div>years</div>
+            </div>
+          </div>
+        </div>
+        <div className='InputData-addDataBlock-int'>
+          <div className='InputData-addDataBlock-int-title'>Interest Rate</div>
+          <div className='InputData-addDataBlock-int-inputBlock'>
+            <input className='InputData-addDataBlock-int-inputBlock-input'></input>
+            <div className='InputData-addDataBlock-int-inputBlock-percent'>%</div>
+          </div>
+        </div>
+      </div>
+
+      <div className='InputData-type'>
+        <button>Repayment</button>
+        <button>Interest Only</button>
+      </div>
+
+      <div className='InputData-buttonBLock'>
+        <img src={calcImg}></img>
+        <div className='InputData-buttonBLock-text'>Calculate Repayments</div>
+      </div>
+      {/*<button onClick={changeFormShowData}>Calculate Repaymants</button>*/}
     </div>
   )
 }
